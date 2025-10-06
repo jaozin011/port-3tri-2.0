948ab20b2fff5b8c7b2df88ba5d273fe8de99b11
jaozin011 <140723111+jaozin011@users.noreply.github.com>
2025-10-06 08:20:23 -0300

    Add files via upload

---
+++ Finalizado-Portfolio-main/Portfolio Hub/app.js
@@ -0,0 +1,44 @@
+const video1 = document.getElementById('projectVideo1');
+const video2 = document.getElementById('projectVideo2');
+const video3 = document.getElementById('projectVideo3');
+const video4 = document.getElementById('projectVideo4');
+const video5 = document.getElementById('projectVideo5');
+const video6 = document.getElementById('projectVideo6');
+const video7 = document.getElementById('projectVideo7');
+const video8 = document.getElementById('projectVideo8');
+
+// Sidebar elements //
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
+
+const hoverSign = document.querySelector('.hover-sign');
+
+const videoList =[video1, video2, video3, video4, video5, video6, video7, video8];
+
+videoList.forEach (function(video){
+    video.addEventListener("mouseover", function(){
+        video.play()
+        hoverSign.classList.add("active")
+    })
+    video.addEventListener("mouseout", function(){
+    video.pause();
+    hoverSign.classList.remove("active")
+})
+})
+
+// Sidebar elements //
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+});
+
+
+
